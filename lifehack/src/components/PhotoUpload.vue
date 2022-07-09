5bb33af4e2b270dc13e0f44ffd44f412af38c8f1
thomastan99 <71714178+thomastan99@users.noreply.github.com>
2022-07-09 16:53:36 +0800

    Photo Upload

--- lifehack/src/components/PhotoUpload.vue
+++ lifehack/src/components/PhotoUpload.vue
@@ -1,18 +1,38 @@
 <template>
-  <div>
+    <form @submit.prevent > 
+         <h3 class = "heading"> Please ensure that the photos are in JPEG format !</h3>
+  <div class = "before">
+   
     <div
+        
       class="imagePreviewWrapper"
       :style="{ 'background-image': `url(${previewImage})` }"
       @click="selectImage">
+   
     </div>
- 
     <input
       ref="fileInput"
       type="file"
       @input="pickFile">
+       <h3>Pre-Meal Photo</h3>
   </div>
-
-    <button @click="sendtoFB">Upload</button>
+    <div class = "before">
+    <div
+      class="imagePreviewWrapper"
+      :style="{ 'background-image': `url(${previewImagee})` }"
+      @click="selectImage2">
+    
+    </div>
+    <input
+      ref="fileInput2"
+      type="file"
+      @input="pickFile2">
+        <h3>Post-Meal Photo</h3>
+  </div>
+    <br><br>
+    <input id="button" type ="submit" value = "Upload" v-on:click="sendtoFB">
+    </form>
+    <!-- <button @click="sendtoFB">Upload</button> -->
 </template>
  
 <script>
@@ -24,7 +44,8 @@ const db = getFirestore(firebaseApp)
 export default {
   data() {
       return {
-        previewImage: null
+        previewImage: null,
+        previewImagee: null
       };
     },
   methods: {
@@ -43,14 +64,41 @@ export default {
           this.$emit('input', file[0])
         }
       },
+            pickFile2 () {
+        let input = this.$refs.fileInput2
+        let file = input.files
+        if (file && file[0]) {
+          let reader = new FileReader
+          reader.onload = e => {
+            this.previewImagee = e.target.result
+          }
+          reader.readAsDataURL(file[0])
+          this.$emit('input', file[0])
+        }
+      },
+            selectImage2 () {
+          this.$refs.fileInput2.click()
+      },
       sendtoFB() {
         const pic = {
-            pic: this.previewImage,
+            before: String(this.previewImage),
+            after: String(this.previewImagee),
             date: Date.now()
         }
+        if(this.previewImage == null || this.previewImagee == null){
+            alert("Please upload both images before submitting!")
+        }
+        else{
         addDoc(collection(db, "users", "thomthom", "pics" ), pic )//String(this.previewImage))
         console.log("done")
-      }
+        alert("Pictures Successfully Uploaded!")
+        this.previewImage = null
+        this.previewImagee = null
+        }
+    
+
+      },
+
   }
 }
 </script>
@@ -64,5 +112,16 @@ export default {
     margin: 0 auto 30px;
     background-size: cover;
     background-position: center center;
+}
+
+.before{
+    display: inline-block;
+    padding: 100px;
+}
+.after{
+    display: inline-block;
+}
+.heading{
+    text-align: center;
 }
 </style>
