3fc549944b3e113c31e4774b883b4af5261ea959
melissaoyl <yilinmelissa@gmail.com>
2022-07-09 17:05:14 +0800

    photo upload merge

--- lifehack/src/router/index.js
+++ lifehack/src/router/index.js
@@ -1,7 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import LoginPage from "@/views/LoginPage.vue";
 import RegistrationPage from "@/views/RegistrationPage.vue";
-import LoginPage from "@/components/LoginPage.vue";
 import PhotoUpload from "@/components/PhotoUpload.vue"
 
 const routes = [
